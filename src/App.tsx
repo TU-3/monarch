56edcef6920dccc7c1a3f5ba49688e69656cb6a7
Aashna-Verma <aashna.verma@outlook.com>
2025-04-28 21:23:50 -0400

    bug(infra): path alias direct correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button"
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -18,9 +19,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -28,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
     </>
   )
 }
