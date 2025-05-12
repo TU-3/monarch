86bcf89e14114c18e3f29bebf0f7277b37e98b38
Tharusha Herath <tharusha.herath13@gmail.com>
2025-05-12 14:26:47 -0400

    fix: Import relations as part of schema in order to fix relationship querying.

--- backend/db/index.ts
+++ backend/db/index.ts
@@ -3,10 +3,10 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import { config } from 'dotenv';
 import * as schema from "./schema"; // <--- your tables
-import "./relations";              // <--- this is key
+import * as relations from "./relations";
 
 config(); // load env vars
 
 
 const client = postgres(process.env.DATABASE_URL!, { prepare: false });
-export const db = drizzle(client, { schema });
+export const db = drizzle(client, { schema:  { ...schema, ...relations }});
